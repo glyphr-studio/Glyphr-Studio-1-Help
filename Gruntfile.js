736aff5f090c11dbc8d0e65eed39bfb85913f19c
mattlag <mattlag@gmail.com>
2015-02-02 15:40:35 -0800

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+	// mainfest variable will be used to generate
+	// the left hand nav. Underscores will be stripped
+	// to spaces. # Will be converted to section titles.
+
+	var manifest = [
+		'#Overview',
+		'starting_a_new_project',
+		'navigation_and_layout',
+		'common_character_editing_pages',
+		
+		'#Editing',
+		'shape_editing',
+		'canvas_tools',
+		'keyboard_shortcuts',
+
+		'#Panels',
+		'character_panel',
+		'shape_panel',
+		'path_point_panel',
+		'linked_shape_instance_panel',
+		'edit_history_panel',
+		'guides_panel',
+
+		'#Pages',
+		'ligatures_page',
+		'kerning_page',
+		'test_drive_page',
+		'font_settings_page',
+		'project_settings_page',
+		'import_svg_page',
+		'export_font_page',
+		'about_page'
+	];
+
+
+	// Generate the left hand navigation
+	var nav = '';
+	var man = '';
+	for (var i = 0; i < manifest.length; i++) {
+		man = manifest[i];
+
+		if(man.charAt(0) === '#'){
+			nav += '<h1>'+man.substr(1)+'</h1>\n';
+		} else {
+			nav += '<a href="'+man+'.html">'+man.replace(/_/gi, ' ')+'</a>\n';
+		}
+	}
+
+	var bannerhtml = grunt.file.read('page_pieces/top.htm');
+	bannerhtml = bannerhtml.replace(/{{nav}}/, nav);
+	var footerhtml = grunt.file.read('page_pieces/bottom.htm');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				banner: bannerhtml,
+				footer: footerhtml
+			},
+			build: {
+				files: [{
+					expand: true,		// Enable dynamic expansion.
+					cwd: 'articles/',	// Src matches are relative to this path.
+					src: ['*.htm'],		// Actual pattern(s) to match.
+					dest: 'build/',		// Destination path prefix.
+					ext: '.html',		// Dest filepaths will have this extension.
+					extDot: 'first'		// Extensions in filenames begin after the first dot
+				}]
+			}
+		}
+	});
+
+	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// Tasks
+	grunt.registerTask('default', ['concat']);
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Glyphr Studio Help
+This is a repository for Glyphr Studio help articles.
+
+## Glyphr Studio
+Glyphr Studio is a free, web based font designer, focusing on font design hobbyists.  
+More information can be found at [glyphrstudio.com](http://glyphrstudio.com) - and/or follow [@glyphrstudio](https://twitter.com/glyphrstudio) on twitter.
+
+## License
+Copyright (C) 2015 Matthew LaGrandeur, released under [GPL 3.0](https://github.com/mattlag/Glyphr-Studio-Help/blob/master/LICENSE-gpl-3.0.txt)
+
+## Author
+| ![Matthew LaGrandeur's picture](https://1.gravatar.com/avatar/f6f7b963adc54db7e713d7bd5f4903ec?s=70) |
+|---|
+| [Matthew LaGrandeur](http://mattlag.com/) |
+| matt[at]mattlag[dot]com |
+
+
+
